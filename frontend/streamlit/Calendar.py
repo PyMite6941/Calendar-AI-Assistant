04cd891f3658ed17b9b39bd7fae776230965b380
-XO-OX- <greshamd27@gmail.com>
2026-05-30 19:48:57 +0700

    Quick updates

--- frontend/streamlit/Calendar.py
+++ frontend/streamlit/Calendar.py
@@ -1,7 +1,11 @@
 # Modules for functionality
 import streamlit as st
 from streamlit_calendar import calendar
+import sys
+from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(PROJECT_ROOT))
 from backend.tools.calendar_events import get_calendar_events
 
 pages = ["Description", "Calendar", "Todo", "Settings"]
--- frontend/streamlit/Description.py
+++ frontend/streamlit/Description.py
@@ -1,7 +1,20 @@
 # Modules for functionality
+import os
 import streamlit as st
+from streamlit_option_menu import option_menu
+import sys
+from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(PROJECT_ROOT))
 from backend.tools.calendar_events import get_calendar_events
+
+description_page = st.Page("Description.py", title="Description", icon=":material/info:")
+calendar_page = st.Page("Calendar.py", title="Calendar", icon=":material/chat:")
+todo_page = st.Page("Todo.py", title="Todo", icon=":material/list-task:")
+settings_page = st.Page("Settings.py", title="Settings", icon=":material/settings:")
+pg = st.navigation([description_page, calendar_page, todo_page, settings_page])
+pg.run()
 
 if not st.session_state.get("initialized", False):
     st.session_state["initialized"] = True
