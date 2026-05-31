f7c08a0739e759cb83fda1f4789609f8787d8bd7
-XO-OX- <greshamd27@gmail.com>
2026-05-31 08:00:26 +0700

    Quick changes

--- frontend/streamlit/Settings.py
+++ frontend/streamlit/Settings.py
@@ -1,2 +1,12 @@
 # Modules for functionality
 import streamlit as st
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(PROJECT_ROOT))
+from backend.tools.calendar_events import get_calendar_events
+
+if not st.session_state.get("initialized", False):
+    st.session_state["initialized"] = True
+    st.session_state["calendar"] = get_calendar_events()
